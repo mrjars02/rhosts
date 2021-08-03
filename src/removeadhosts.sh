621e9ebb2fec9bd3155277be86de5ef7bf92d833
Justin Reichardt <mrjars@gmail.com>
2021-08-03 15:06:17 -0500

    Retain adlistings if nothing new is pulled
    This way if either you have no internet or all of your listing no longer
    exist you will not lose everything

--- src/removeadhosts.sh
+++ src/removeadhosts.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sed -n '1,/# start of removeadhosts/p' /etc/hosts > /tmp/removeadhosts
-cat /tmp/removeadhosts | tee /etc/hosts >/dev/null
-rm /tmp/removeadhosts
+sed -n '1,/# start of removeadhosts/p' /etc/hosts > /tmp/removeadhosts-head
+sed -n '/# start of removeadhosts/,/# Custom ad list/p' /etc/hosts | sed -e '/# Custom ad list/d' -e '1d' > /tmp/removeadhosts-curl
+cat /tmp/removeadhosts-head | tee /etc/hosts >/dev/null
+rm /tmp/removeadhosts-head
 echo "Appended old hosts"
 if grep -qinE '# start of removeadhosts' /etc/hosts
 then
@@ -12,7 +13,6 @@ else
 fi
 
 echo 'Downloading ad list'
-
 if [ -e /etc/removeadhosts/adlistings.txt ]
 then
         cat /etc/removeadhosts/adlistings.txt | \
@@ -21,6 +21,13 @@ then
             echo "0.0.0.0 $CMD" >> /etc/hosts
         done
 fi
+
+if [ $(sed -n '/# start of removeadhosts/,/# Custom ad list/p' /etc/hosts | sed -e '/# Custom ad list/d' -e '1d' | wc -l) -lt 2 ]
+then
+        echo "No hosts were downloaded, reusing the old ones"
+        cat /tmp/removeadhosts-curl | tee -a /etc/hosts >/dev/null
+fi
+rm /tmp/removeadhosts-curl
 
 echo 'Adding custom items from /etc/removeadhosts'
 if [ -e /etc/removeadhosts/adlist.txt ]
