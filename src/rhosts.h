1c6d230aacb0e456b3bda0298a4a19ff34db146a
Justin Reichardt <mrjars@gmail.com>
2021-08-26 19:08:35 -0500

    Forgot to stage the function definition

--- src/rhosts.h
+++ src/rhosts.h
@@ -45,4 +45,5 @@ int download_entries(struct entry **entries);
 int add_site_entries(struct entry **entries);
 int download_libcurl(char *e);
 int parse_download(char *buff, size_t size, size_t nmemb);
+int copy_old_download(char *url);
 #endif
