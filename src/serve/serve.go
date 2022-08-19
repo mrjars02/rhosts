75207c51df5da09ec1eb5f53357171338d322942
Justin Reichardt <mrjars@gmail.com>
2022-08-19 12:28:02 -0500

    Added a simple http server
    This will later be used for serving false content

---
+++ src/serve/serve.go
@@ -0,0 +1,25 @@
+// Provides the web server for rhosts to relay altered content
+package serve
+
+import(
+	"net/http"
+)
+
+func Start(certLoc string) {
+	go httpServer()
+	//go httpsServer(&certLoc)
+
+}
+
+func httpServer() (err error){
+	err = http.ListenAndServe("127.0.0.1:80",http.HandlerFunc(httpHandler))
+	return
+}
+func httpsServer(certLoc *string) (err error){
+	err = http.ListenAndServeTLS("127.0.0.1:80",*certLoc + "ca.crt", *certLoc + "ca.key",http.HandlerFunc(httpHandler))
+	return
+}
+
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w,"Test",200)
+}
